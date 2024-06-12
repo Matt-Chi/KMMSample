8ea440e2dea074d0c6ecf83c2232f785645cf226
Matt-Chi <81298116+Matt-Chi@users.noreply.github.com>
2024-06-12 13:07:59 +0800

    Display a string list
    - Modify display text to string list.

--- androidKmm/src/main/java/com/example/kmm_test/android/MainActivity.kt
+++ androidKmm/src/main/java/com/example/kmm_test/android/MainActivity.kt
@@ -3,11 +3,16 @@ package com.example.kmm_test.android
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.kmm_test.Greeting
 
 class MainActivity : ComponentActivity() {
@@ -19,7 +24,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    GreetingView(Greeting().greet())
+                    GreetingView()
                 }
             }
         }
@@ -27,14 +32,23 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun GreetingView(text: String) {
-    Text(text = text)
+fun GreetingView() {
+    val greeting = remember { Greeting().greet() }
+    Column(
+        modifier = Modifier.padding(all = 20.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp),
+    ) {
+        greeting.forEach {
+            Text(it)
+            Divider()
+        }
+    }
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MyApplicationTheme {
-        GreetingView("Hello, Android!")
+        GreetingView()
     }
 }
